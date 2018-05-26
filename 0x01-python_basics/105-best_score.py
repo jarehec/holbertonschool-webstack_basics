4aaf4d24f3ee70f1561c31b8b681e87622d822c5
jarehec <bone1198@gmail.com>
2018-05-26 14:15:01 -0700

    disallow 0 length dicts

--- 0x01-python_basics/105-best_score.py
+++ 0x01-python_basics/105-best_score.py
@@ -9,7 +9,7 @@ def best_score(my_dict):
     returns a key with the biggest integer value
     @my_dict: dictionary with integer values
     """
-    if my_dict is not None:
+    if my_dict is not None and len(my_dict) > 0:
         val = max(my_dict.values())
         for k, v in my_dict.items():
             if v == val:
