27d92346d560659a6619961e46fa1a8c59a7df16
jarehec <bone1198@gmail.com>
2018-05-26 09:33:12 -0700

    verbose initialization

--- 0x01-python_basics/4-add.py
+++ 0x01-python_basics/4-add.py
@@ -2,9 +2,10 @@
 """
 module that imports the function def add(a, b)
 """
+from add_4 import add
 
 
 if __name__ == '__main__':
-    from add_4 import add
-    a, b = 1, 2
-    print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
+    a = 1
+    b = 2
+    print('{} + {} = {}'.format(a, b, add(a, b)))
